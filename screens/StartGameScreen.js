2b11db721459880d11675dbfe8a8b5625e2fde76
Ebo Arcadia <yiboli.coding@gmail.com>
2023-07-13 16:17:40 -0400

    first commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,11 @@
+import { TextInput, View } from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput>Start the game button</TextInput>
+    </View>
+  );
+}
+
+export default StartGameScreen;
