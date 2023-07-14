342a3a2d292a9fffb361dc39185a62dff59f44a2
Ebo Arcadia <yiboli.coding@gmail.com>
2023-07-14 11:49:32 -0400

    custom style start game sreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,29 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 20,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 10,
+    elevation: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 10, height: 10 },
+    shadowRadius: 7,
+    shadowOpacity: 0.9,
+  },
+});
 
 export default StartGameScreen;
