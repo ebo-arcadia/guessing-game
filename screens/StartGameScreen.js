b149c87691779b2c67befe138c6df804bfce824a
Ebo Arcadia <yiboli.coding@gmail.com>
2023-07-16 10:41:24 -0400

    improve button design

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,14 +11,22 @@ function StartGameScreen() {
         autoCapitalize="none"
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 20,
     padding: 16,
@@ -41,6 +49,12 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
 
